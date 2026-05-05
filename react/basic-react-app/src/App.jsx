ec2da980201368d434b4bce98f8c6a0beed7dd6e
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-05-06 01:59:00 +0530

    creating first react component

--- react/basic-react-app/src/App.jsx
+++ react/basic-react-app/src/App.jsx
@@ -1,10 +1,28 @@
-import "./App.css"
+import "./App.css";
 
-function App() {
-  
-
-  // return <h1>Hello World ! </h1>;
-  return <button>hello world!</button>;
+function Title() {
+  return <h1>I m Dany !</h1>;
+}
+function Description(){
+  return <h3>Im the description !</h3>
 }
 
-export default App
+function App() {
+  // return <h1>Hello World ! </h1>;
+  // return <button>hello world!</button>;
+
+  return (
+    <div>
+      <h1>This is my App component </h1>
+      <p>inside app component we have :</p>
+      <Title></Title>
+      <Description></Description>
+      <Title></Title>
+      <Description></Description>
+
+    </div>
+  );
+}
+
+export default App;
+// export  Title;
